491eaef46ff3c837743229c3a35dbc4634a3ef95
cebebo <156794007+cebebo@users.noreply.github.com>
2024-03-02 21:10:27 +0100

    Continued fillig code in sign up and login pages

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,33 @@
 let users = [
-    {'email': 'admin', 'password': 'admin'}
+    {'name': 'admin', 'email': 'admin', 'password': 'admin'}
 ];
+
+function signUpUser() {
+    let name = document.getElementById('signupName');
+    let email = document.getElementById('signupEmail');
+    let password = document.getElementById('signupPassword');
+    if (password.value == document.getElementById('signupConfirmPassword').value) {
+        users.push({name: name.value, email: email.value, password: password.value});
+        document.getElementById('sucessBoxSignUp').classList.remove('d-none');
+        name.value = '';
+        email.value = '';
+        password.value = ''
+        document.getElementById('signupConfirmPassword').value = '';
+        window.location.href = 'login.html?msg=Sign up successful';
+    } else { alert('Die Passwörter stimmen nicht überein!'); }    
+}
+
+function logInUser() {
+    let email = document.getElementById('loginEmail');
+    let password = document.getElementById('loginPassword');
+    let user = users.find( u => u.email == email.value && u.password == password.value);
+    if (user) {
+        window.location.href = 'index.html?msg=Login successful';
+    }
+}
+
+const urlMessage = new URLSearchParams(window.location.search);
+const msg = urlMessage.get('msg');
+if (msg) {
+    document.getElementById('sucessBoxSignUp').innerHTML = msg;
+}
