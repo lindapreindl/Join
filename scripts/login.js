4db85ea17a330a51e00ce902fe8ff4f8b6e452df
cebebo <156794007+cebebo@users.noreply.github.com>
2024-03-04 12:37:32 +0100

    added notifications in login und signup

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,16 @@
 let users = [];
+let rememberUser;
 
 async function initUsers() {
     loadUsers();
+    rememberUser = localStorage.getItem('rememberUser');
+    if (rememberUser) { document.getElementById('loginEmail').value = rememberUser; }
 }
 
 async function loadUsers() {
     try {
         users = JSON.parse(await getItem('users'));
-    } catch(e) {
+    } catch (e) {
         console.info('No users found.');
     }
 }
@@ -17,12 +20,16 @@ async function signUpUser() {
     let email = document.getElementById('signupEmail');
     let password = document.getElementById('signupPassword');
     if (password.value == document.getElementById('signupConfirmPassword').value) {
-        users.push({name: name.value, email: email.value, password: password.value});
+        users.push({ name: name.value, email: email.value, password: password.value });
         await setItem('users', JSON.stringify(users));
-        // document.getElementById('sucessBoxSignUp').classList.remove('d-none');
         resetForm(name, email, password)
         window.location.href = 'login.html?msg=Sign up successful';
-    } else { alert('Die Passwörter stimmen nicht überein!'); }    
+    } else { noMatchedPasswords() }
+}
+
+function noMatchedPasswords() {
+    document.getElementById('signupConfirmPassword').style.borderColor = "#FD1E39";
+    document.getElementById('passwordNoMatch').classList.remove('d-none');
 }
 
 function resetForm(name, email, password) {
@@ -35,15 +42,26 @@ function resetForm(name, email, password) {
 function logInUser() {
     let email = document.getElementById('loginEmail');
     let password = document.getElementById('loginPassword');
-    let user = users.find( u => u.email == email.value && u.password == password.value);
+    let user = users.find(u => u.email == email.value && u.password == password.value);
     if (user) {
+        if (document.getElementById('rememberMeCheck').value = true) {
+            localStorage.setItem('rememberUser', email.value);
+        }
         window.location.href = 'index.html?msg=Login successful';
-    }
+    } else { wrongPassword() }
+}
+
+function wrongPassword() {
+    document.getElementById('loginPassword').style.borderColor = "#FD1E39";
+    document.getElementById('wrongPassword').classList.remove('d-none');
 }
 
 
-// const urlMessage = new URLSearchParams(window.location.search);
-// const msg = urlMessage.get('msg');
-// if (msg) {
-//     document.getElementById('sucessBoxSignUp').innerHTML = msg;
-// }
+function message() {
+    const urlMessage = new URLSearchParams(window.location.search);
+    const msg = urlMessage.get('msg');
+    if (msg) {
+        document.getElementById('sucessBoxSignUp').classList.remove('d-none');
+        document.getElementById('sucessBoxSignUp').innerHTML = msg;
+    }
+}
