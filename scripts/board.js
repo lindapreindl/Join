d0b15e97d144c558c57a4141a46eba679edf39f0
cebebo <156794007+cebebo@users.noreply.github.com>
2024-03-06 21:54:22 +0100

    Designed the tasks in board

--- scripts/board.js
+++ scripts/board.js
@@ -2,58 +2,73 @@ let tasks = [
     {
         'titel': 'Zwiebeln schneiden',
         'description': 'Rote Zwiebeln in kleine Würfel schneiden',
-        'assigned': ['test'],
+        'assigned': ['CB', 'GV', 'PP'],
         'dueDate': '24.12.2024',
-        'prio': 'test',
+        'prio': 'urgent',
         'category': 'User Story',
-        'position': 'InProgress',
-        'subtasks': [{
-            'subtask': 'test',
-            'finished': false
-        }]
+        'position': 'ToDo',
+        'subtasks': [
+            {
+                'subtask': 'Schäler spülen',
+                'finished': true
+            },
+            {
+                'subtask': 'Zwiebeln schälen',
+                'finished': true
+            },
+            {
+                'subtask': 'Zwiebeln würfeln',
+                'finished': false
+            }
+        ]
     },
     {
         'titel': 'Holz hacken',
         'description': 'Für ein nettes, kleines Lagerfeuer',
-        'assigned': ['test'],
+        'assigned': ['JC'],
         'dueDate': '24.12.2024',
-        'prio': 'test',
+        'prio': 'medium',
         'category': 'User Story',
         'position': 'InProgress',
-        'subtasks': [{
-            'subtask': 'test',
-            'finished': false
-        }]
+        'subtasks': [
+            {
+                'subtask': 'Axt schärfen',
+                'finished': true
+            },
+            {
+                'subtask': 'Holz spalten',
+                'finished': false
+            }
+        ]
     },
     {
         'titel': 'Join programmieren',
         'description': 'Ein cooles Programm schreiben zur Arbeitsplanung und Aufgabenverteilung in einem Team',
-        'assigned': ['test'],
+        'assigned': ['AB', 'CD', 'EF', 'GH', 'JI', 'KL'],
         'dueDate': '24.12.2024',
-        'prio': 'test',
+        'prio': 'low',
         'category': 'Technical Task',
         'position': 'InProgress',
         'subtasks': [{
-            'subtask': 'test',
+            'subtask': 'Brainstorming',
             'finished': false
         }]
     },
     {
         'titel': 'Nase putzen',
         'description': 'Mit einem Taschentuch die Atemwege reinigen',
-        'assigned': ['test'],
+        'assigned': ['MN'],
         'dueDate': '24.12.2024',
-        'prio': 'test',
+        'prio': 'low',
         'category': 'Technical Task',
-        'position': 'InProgress',
-        'subtasks': [{
-            'subtask': 'test',
-            'finished': false
-        }]
+        'position': 'AwaitFeedback',
+        'subtasks': ''
     }
 ];
 let categoryColors = ['#0038FF', '#1FD7C1']
 let draggedTask;
+let currentSubtaskDone;
+let currentSubtaskProgress;
 
 async function initBoard() {
     // let loadedTasks = getItem(tasks);
@@ -66,7 +81,11 @@ function fillBoardWithTasks() {
     for (let i = 0; i < tasks.length; i++) {
         let process = tasks[i].position;
         let categoryColor = getCategoryColor(tasks[i].category);
-        document.getElementById(`content${process}`).innerHTML += templateTasksInBoard(i, categoryColor);
+        let currentSubtasks = checkSubtasks(tasks[i].subtasks, i);
+        let prio = `${tasks[i].prio}.png`
+        document.getElementById(`content${process}`).innerHTML += templateTasksInBoard(i, categoryColor, currentSubtasks, prio);
+        fillAssignedStaff(i);
+        if (tasks[i].subtasks.length == 0) { document.getElementById(`taskProgress${i}`).classList.add('d-none'); }
     }
     checkForEmptyColums()
 }
@@ -76,6 +95,30 @@ function getCategoryColor(category) {
     if (category == "User Story") { result = categoryColors[0] };
     if (category == "Technical Task") { result = categoryColors[1] };
     return result;
+}
+
+function checkSubtasks(subtasks, i) {
+    let amount = subtasks.length;
+    let tasksDone = culcualteSubtasks(amount, i);
+    if (amount > 0) { 
+        currentSubtaskProgress = (100 / amount) * tasksDone;
+    }
+    return amount;
+}
+
+function culcualteSubtasks(amount, i) {
+    let amountDone = 0;
+    for (let j = 0; j < amount; j++) {
+        if (tasks[i].subtasks[j].finished === true) { amountDone++; }
+    }
+    currentSubtaskDone = amountDone;
+    return amountDone;
+}
+
+function fillAssignedStaff(i) {
+    for (let j = 0; j < tasks[i].assigned.length; j++) {
+        document.getElementById(`assign${i}`).innerHTML += templateTasksAssignedStaff(i, j);
+    }
 }
 
 function clearBoard() {
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,12 +1,19 @@
-function templateTasksInBoard(i, categoryColor) {
+function templateTasksInBoard(i, categoryColor, subtasks, prio) {
     return `
     <div draggable="true" ondragstart="startDragging(${i})" class="popUpBox widthTaskBox" id="task${i}">
         <div class="categoryLine">
             <div class="categoryBox" style="background-color:${categoryColor}">${tasks[i].category}</div>
         </div>
-        <h3>${tasks[i].titel}</h3>
-        <p>${tasks[i].description}</p>
-
+        <h3 class="mb12">${tasks[i].titel}</h3>
+        <p class="mb24 fontLightgrey">${tasks[i].description}</p>
+        <div id="taskProgress${i}" class="w100 d-flex align-center justify-between mb24">
+            <progress value="${currentSubtaskProgress}" max="100"></progress>
+            <p style="font-size:12px;">${currentSubtaskDone}/${subtasks} Subtasks</p>
+        </div>
+        <div class="d-flex justify-between align-center w100">
+            <div id="assign${i}" class="d-flex"></div>
+            <div><img class="prioItem" src="../img/${prio}"></div>
+        </div>
     </div>
     `
 }
@@ -15,4 +22,8 @@ function noTasksPlaced(process) {
     return `
     <div class="emptyTask fontLightgrey">No tasks ${process}</div>
     `;
+}
+
+function templateTasksAssignedStaff(i, j) {
+    return `<div class="assigneInitials">${tasks[i].assigned[j]}</div>`;
 }
