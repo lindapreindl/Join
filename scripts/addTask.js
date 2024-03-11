08ffdbddd65e38742e5f3fdd54fd7978e36f6cea
Linda Preindl <lindapreindl@gmail.com>
2024-03-11 07:23:47 +0100

    added functions to addTask

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -1,3 +1,41 @@
-function openAddTaskInBoard(){
+function openAddTaskInBoard() {
     document.getElementById('addTaskInBoard').classList.remove('d-none');
+}
+
+
+function closeAddTaskInBoard() {
+    document.getElementById('addTaskInBoard').classList.add('d-none');
+}
+
+
+/*function clearTaskInBoard() {
+    document.getElementById('titleAddTaskInBoard').innerHTML = '';
+    document.getElementById('descriptionAddTaskInBoard').innerHTML = '';
+    document.getElementById('assignedToAddTaskInBoard').innerHTML = '';
+    document.getElementById('dueDateAddTaskInBoard').innerHTML = '';
+    document.getElementById('categoryAddTaskInBoard').innerHTML = '';
+    document.getElementById('subtasksAddTaskInBoard').innerHTML = '';
+}*/
+
+
+function createTaskInBoard() {
+    let newTitle = document.getElementById('titleAddTaskInBoard').value;
+    let newDescription = document.getElementById('descriptionAddTaskInBoard').value;
+    let newAssignedTo = document.getElementById('assignedToAddTaskInBoard').value;
+    let newDueDate = document.getElementById('dueDateAddTaskInBoard').value;
+    let newCategory = document.getElementById('categoryAddTaskInBoard').value;
+    let newSubtask = document.getElementById('subtasksAddTaskInBoard').value;
+
+    console.log(newTitle, newDescription, newAssignedTo, newDueDate, newCategory, newSubtask);
+
+    let oldLength = tasks.length;
+    let newLength = oldLength + 1;
+    console.log(newLength);
+
+    tasks[newLength]['titel'].push(newTitle);
+    tasks[newLength]['description'].push(newDescription);
+    tasks[newLength]['assigned'].push(newAssignedTo);
+    tasks[newLength]['dueDate'].push(newDueDate);
+    tasks[newLength]['category'].push(newCategory);
+    tasks[newLength]['subtasks'].push(newSubtask);
 }
