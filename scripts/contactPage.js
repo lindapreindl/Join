405fa472b2b7443d64fd536655b38f768093bdef
Linda Preindl <lindapreindl@gmail.com>
2024-04-03 16:50:54 +0200

    reset code correctly

--- scripts/contactPage.js
+++ scripts/contactPage.js
@@ -2,18 +2,14 @@
 users = [];
 // Objekt zur Speicherung der Farben für jeden Benutzer
 let userColors = {};
-// Variable zum Speichern des kleinen Fensters/Overlays
-let smallOverlay = null;
+
 // Variable zur Speicherung des Overlay-Elements
 let overlay;
-let userInfoOverlay; // Globale Variable für das Overlay
 
 // Funktion zum Initialisieren der Anwendung; lädt Benutzerdaten und rendert die Benutzerinformationen
 async function initContacts() {
   await loadUsers(); // Lade Benutzerdaten
   renderUsersList(); // Rendere Benutzerinformationen
-  // Seite initialisieren
-  adjustVisibility();
 }
 /* ———————————————————————————————————————————————————————————— */
 /* // Funktion zum Öffnen des Overlays
@@ -44,41 +40,47 @@ function createOverlay() {
   overlay.style.display = "none";
   overlay.innerHTML = /* HTML */ `
     <div class="overlay-content1">
-      <div><img class="editLook" src="./img/addContactPic.png" alt="" /></div>
-      <div><img class="addPersonPic" src="./img/addPerson.png" alt="" /></div>
+      <div class="contact-popup-left"> <img src="./img/logo_white.png" alt="">
+            <h1>Add contact</h1>
+            <h3>Tasks are better with a Team!</h3>
+            <div class="blue-line-add-Contact"></div>
+      </div>
+      <div class="addPersonPic"><img src="./img/addPerson.png" alt="" /></div>
 
       <div class="addUserDaten">
         <div class="imgClose" onclick="closeOverlay()">
           <img src="./img/close.png" alt="" />
         </div>
-        <input
-          class="iconPerson"
-          type="text"
-          id="newUserName"
-          placeholder="Name"
-          required
-        /><br /><br />
-        <input
-          class="iconMail"
-          type="email"
-          id="newUserEmail"
-          placeholder="Email"
-          required
-        /><br /><br />
-        <input
-          class="iconCall"
-          type="text"
-          id="newUserPhone"
-          placeholder="Phone"
-          required
-        /><br /><br />
-        <div class="cancel-createContact-btn ">
-          <button class="cancel-btn" onclick="closeOverlay()">
-            Cancel <img class="imgCancel" src="./img/cancel.png" alt="" />
-          </button>
-          <button onclick="addNewUser()" class="check-btn ">
-            Create contact <img class="imgCheck" src="./img/check.png" alt="" />
-          </button>
+        <div class="center">
+          <input
+            class="iconPerson"
+            type="text"
+            id="newUserName"
+            placeholder="Name"
+            required/>
+
+          <input
+            class="iconMail"
+            type="email"
+            id="newUserEmail"
+            placeholder="Email"
+            required/>
+
+          <input
+            class="iconCall"
+            type="text"
+            id="newUserPhone"
+            placeholder="Phone"
+            required/>
+
+          <div class="cancel-createContact-btn ">
+            <button class="cancel-btn" onclick="closeOverlay()">
+              Cancel <img class="imgCancel" src="./img/cancel.png" alt="" />
+            </button>
+            <button onclick="addNewUser()" class="check-btn ">
+              Create contact <img class="imgCheck" src="./img/check.png" alt="" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -91,37 +93,6 @@ function closeOverlay() {
   overlay.style.display = "none";
   overlay.remove(); // Overlay aus dem DOM entfernen
 }
-// Funktion zum Generieren einer eindeutigen Farbe für einen neuen Benutzer
-function generateUniqueColor() {
-  // Array mit allen verfügbaren Farben
-  const allColors = [
-    "#FF5733",
-    "#FFC300",
-    "#DAF7A6",
-    "#9AECDB",
-    "#A3E4D7",
-    "#85C1E9",
-    "#A569BD",
-    "#F1948A",
-    "#B2BABB",
-    "#F0B27A",
-  ];
-
-  // Array mit den Farben, die bereits von Benutzern verwendet werden
-  const usedColors = Object.values(userColors);
-
-  // Filtere die verfügbaren Farben und entferne die, die bereits verwendet werden
-  const availableColors = allColors.filter(
-    (color) => !usedColors.includes(color)
-  );
-
-  // Wähle eine zufällige verfügbare Farbe aus
-  const randomColor =
-    availableColors[Math.floor(Math.random() * availableColors.length)];
-
-  return randomColor;
-}
-
 // Funktion zum Hinzufügen eines neuen Benutzers
 async function addNewUser() {
   // Die Werte aus den Eingabefeldern für Name, E-Mail und Telefonnummer abrufen
@@ -131,7 +102,7 @@ async function addNewUser() {
 
   /* NEU 1 */
   const password = ""; // Ein leeres Passwort wird vorerst definiert
-  const color = generateUniqueColor();
+  const color = getColorForUser();
 
   // Überprüfen, ob alle Felder ausgefüllt sind
   if (name && email && phone) {
@@ -269,213 +240,31 @@ function createInitialsCircle(initials, color) {
   return circle;
 }
 
+// Funktion zum Generieren einer Farbe basierend auf dem Benutzernamen
+function getColorForUser(username) {
+  // Sicherstellen, dass der Benutzername vorhanden ist
+  if (!username) {
+    return "#000000"; // Rückgabe einer Standardfarbe, wenn kein Benutzername angegeben ist
+  }
+
+  // Berechne einen numerischen Hash-Wert aus dem Benutzernamen
+  let hash = 0;
+  for (let i = 0; i < username.length; i++) {
+    hash = username.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  // Konvertiere den Hash-Wert in eine hexadezimale Farbe
+  let color = "#";
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += ("00" + value.toString(16)).substr(-2);
+  }
+  return color;
+}
 /* ---------------test */
 
 /* ------------- */
 
-// Funktion zum Rendern der Benutzerinformationen mit Bearbeitungsfunktion
-function createUserInfoOverlay(user, contactIndex) {
-  const userInfoOverlay = document.createElement("div");
-  userInfoOverlay.className = "overlay3"; // Stilklasse für Overlay
-  userInfoOverlay.id = "userInfoOverlay"; // Eindeutige ID für das Overlay
-  userInfoOverlay.style.display = "none"; // Verstecke das Overlay standardmäßig
-  userInfoOverlay.style.pointerEvents = "auto"; // Erlaube Mausklicks durch das Overlay
-
-  let frame40HTML = ""; // Variable für das Bild-HTML
-
-  // Überprüfen, ob die Bildschirmbreite zwischen 0 und 430px liegt
-  if (window.innerWidth <= 530) {
-    frame40HTML =
-      '<img class="frame-overlay3"  src="./img/Frame 207.png" alt=""></img>';
-  }
-
-  userInfoOverlay.innerHTML = /* HTML */ `
-    <div class="overlay-content3">
-      <div class="contactsImg-closeImg">
-        <div>${frame40HTML}</div>
-        <div>
-          <img
-            class="imgCloseUserInfo"
-            onclick="closeUserInfoOverlay()"
-            src="./img/arrow-left-line.png"
-            alt=""
-          />
-        </div>
-      </div>
-
-      <!-- Hier deine Benutzerdetails-HTML einfügen -->
-      <div class="userInfo-div">
-        <div class="circle2-user">
-          <div
-            class="initials-circle-info3"
-            style="background-color: ${userColors[user.name]}; color: white;"
-          >
-            ${getInitials(user.name)}
-          </div>
-          <div>
-            <p class="user-name-info">${user.name}</p>
-            <div class="img-info-edit-delete">
-              <img
-                class="img-edit"
-                onclick="openEditOverlay(${contactIndex})"
-                src="./img/edit.png"
-                alt=""
-              />
-              <img
-                class="img-delete"
-                onclick="deleteUser('${user.name}'), closeUserInfoOverlay()"
-                src="./img/Delete contact.png"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-        <!--  -->
-        <div class="user-info-contactInformation">
-          <h2>Contact Information</h2>
-          <div class="user-mail-info">
-            <h5>Email</h5>
-            <a href="#">${user.email}</a>
-          </div>
-          <div>
-            <h5>Phone</h5>
-            <span>${user.phone}</span>
-          </div>
-        </div>
-        <!--  -->
-      </div>
-      <img
-        onclick="openSmallOverlay('${user.name}', ${contactIndex})"
-        class="imgMenuOption"
-        src="./img/Menu Contact options.png"
-        alt=""
-      />
-    </div>
-  `;
-
-  document.body.appendChild(userInfoOverlay); // Füge das Overlay zum Dokument hinzu
-}
-
-// Funktion zum Anzeigen des UserInfo-Overlays
-function showUserInfoOverlay() {
-  const userInfoOverlay = document.getElementById("userInfoOverlay");
-  if (userInfoOverlay) {
-    userInfoOverlay.style.display = "block"; // Zeige das Overlay an
-  }
-}
-
-// Funktion zum Schließen des UserInfo-Overlays
-function closeUserInfoOverlay() {
-  const userInfoOverlay = document.getElementById("userInfoOverlay");
-  if (userInfoOverlay) {
-    userInfoOverlay.style.display = "none"; // Verstecke das Overlay
-    location.reload(); // Seite neu laden
-  }
-}
-
-/* --------------------------- smallOverlay ------------------------------------ */
-
-// Funktion zum Erstellen des kleinen Fensters/Overlays
-// Funktion zum Erstellen des kleinen Fensters/Overlays
-function createSmallOverlay(contactIndex) {
-  // Erstellen des Overlay-Elements
-  smallOverlay = document.createElement("div");
-  smallOverlay.className = "small-overlay";
-
-  // HTML-Inhalt des kleinen Fensters/Overlays
-  smallOverlay.innerHTML = /* HTML */ `
-    <div class="small-overlay-content">
-      <div class="img-info-edit-delete">
-        <img
-          class="img-edit"
-          onclick="openEditOverlay(${contactIndex})"
-          src="./img/edit.png"
-          alt=""
-        />
-        <img
-          class="img-delete"
-          onclick="deleteUserAtIndex(${contactIndex}), closeSmallOverlay()"
-          src="./img/Delete contact.png"
-          alt=""
-        />
-      </div>
-    </div>
-  `;
-
-  // Anhängen des kleinen Fensters/Overlays an den Body
-  document.body.appendChild(smallOverlay);
-}
-
-// Funktion zum Öffnen des kleinen Fensters/Overlays
-function openSmallOverlay(userName, contactIndex) {
-  // Erstellen und Anzeigen des kleinen Fensters/Overlays
-  createSmallOverlay(userName, contactIndex);
-}
-
-// Funktion zum Schließen des kleinen Fensters/Overlays
-function closeSmallOverlay() {
-  // Überprüfen, ob das kleine Fenster/Overlay vorhanden ist und es entfernen
-  if (smallOverlay) {
-    smallOverlay.remove();
-    smallOverlay = null;
-  }
-}
-/* ----------------------------------------------------------------------- */
-/* redundance von oben */
-// Funktion zum Erstellen des kleinen Fensters/Overlays
-function createSmallOverlay(contactIndex) {
-  // Erstellen des Overlay-Elements
-  smallOverlay = document.createElement("div");
-  smallOverlay.className = "small-overlay";
-
-  // HTML-Inhalt des kleinen Fensters/Overlays
-  smallOverlay.innerHTML = /* HTML */ `
-    <div class="small-overlay-content">
-      <div class="img-info-edit-delete">
-        <img
-          class="img-edit"
-          onclick="openEditOverlay(${contactIndex})"
-          src="./img/edit.png"
-          alt=""
-        />
-        <img
-          class="img-delete"
-          onclick="deleteUserAtIndex(${contactIndex}), closeSmallOverlay()"
-          src="./img/Delete contact.png"
-          alt=""
-        />
-      </div>
-      <img
-        class="close-overlay-icon"
-        src="./img/close-icon.png"
-        alt="Close"
-        onclick="closeSmallOverlay()"
-      />
-    </div>
-  `;
-
-  // Anhängen des kleinen Fensters/Overlays an den Body
-  document.body.appendChild(smallOverlay);
-}
-
-// Funktion zum Öffnen des kleinen Fensters/Overlays
-function openSmallOverlay(contactIndex) {
-  // Erstellen und Anzeigen des kleinen Fensters/Overlays
-  createSmallOverlay(contactIndex);
-}
-
-// Funktion zum Schließen des kleinen Fensters/Overlays
-function closeSmallOverlay() {
-  // Überprüfen, ob das kleine Fenster/Overlay vorhanden ist und es entfernen
-  if (smallOverlay) {
-    smallOverlay.remove();
-    smallOverlay = null;
-  }
-}
-
-/* ------------------------------------------- */
-
-// Funktion zum Rendern der Benutzerinformationen mit Bearbeitungsfunktion
 // Funktion zum Rendern der Benutzerinformationen mit Bearbeitungsfunktion
 function renderUserInfo(user) {
   let contactName = user.name;
@@ -486,41 +275,56 @@ function renderUserInfo(user) {
     <div>
       <img class="frame40" src="./img/contacts.png" alt="" />
     </div>
+    <div class="userInfo-div">
+      <div class="circle2-user">
+        <div
+          class="initials-circle-info"
+          style="background-color: ${userColors[user.name]}; color: white;"
+        >
+          ${getInitials(user.name)}
+        </div>
+        <div>
+          <p class="user-name-info">${user.name}</p>
+          <div class="img-info-edit-delete">
+            <img
+              class="img-edit"
+              onclick="openEditOverlay(${contactIndex})"
+              src="./img/edit.png"
+              alt=""
+            />
+            <img
+              class="img-delete"
+              onclick="deleteUser('${user.name}'), clearUserInfo()"
+              src="./img/Delete contact.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div class="user-info-contactInformation">
+        <h2>Contact Information</h2>
+        <div class="user-mail-info">
+          <h5>Email</h5>
+          <a href="#">${user.email}</a>
+        </div>
+        <div>
+          <h5>Phone</h5>
+          <span>${user.phone}</span>
+        </div>
+      </div>
+      <!--  -->
+
+      <!-- Löschen-Button hinzufügen -->
+      <!-- <hr /> -->
+    </div>
   `;
-
-  // Überprüfen, ob das Overlay bereits existiert
-  if (!userInfoOverlay) {
-    // Wenn das Overlay nicht existiert, erstelle es
-    createUserInfoOverlay(user, contactIndex);
-  } else {
-    // Wenn das Overlay bereits existiert, aktualisiere nur die Benutzerinformationen
-    updateUserInfo(user, contactIndex);
-  }
-
-  // Zeige das Overlay an
-  showUserInfoOverlay();
 }
-
 // Funktion zum Öffnen des Overlays für die Bearbeitung eines Benutzers
 function openEditOverlay(index) {
   const user = users[index]; // Benutzer aus dem Index erhalten
   createEditOverlay(user, index); // Übergebe den Benutzer und den Index an die createEditOverlay-Funktion
   overlay.style.display = "block"; // Overlay anzeigen
-}
-// Funktion zum Aktualisieren der Benutzerinformationen im UserInfo-Overlay
-function updateUserInfo(user, contactIndex) {
-  // Benutzername und Index aktualisieren
-  userInfoOverlay.querySelector(".user-name-info").textContent = user.name;
-  const initialsCircle = userInfoOverlay.querySelector(".initials-circle-info");
-  initialsCircle.textContent = getInitials(user.name);
-  initialsCircle.style.backgroundColor = userColors[user.name];
-  // E-Mail aktualisieren
-  userInfoOverlay.querySelector(".user-mail-info a").textContent = user.email;
-  // Telefonnummer aktualisieren
-  userInfoOverlay.querySelector(
-    ".user-info-contactInformation span"
-  ).textContent = user.phone;
-  renderUserInfo(user);
 }
 
 /* ------------------------ createEditOverlay  --------------------------------- */
@@ -540,12 +344,15 @@ function createEditOverlay(user, index) {
   overlay.style.display = "none";
   overlay.innerHTML = /* HTML */ `
     <div class="overlay-content2">
-      <div><img class="editLook" src="./img/editLook.png" alt="" /></div>
-      <div>
+    <div class="contact-popup-left"> <img src="./img/logo_white.png" alt="">
+            <h1>Edit contact</h1>
+            <h3>Tasks are better with a Team!</h3>
+            <div class="blue-line-add-Contact"></div>
+      </div>
+    <div>
         <div
-          class="initials-circle-info2 initialsEdit"
-          style="background-color: ${userColors[user.name]}; color: white;"
-        >
+          class="initials-circle-info initialsEdit"
+          style="background-color: ${userColors[user.name]}; color: white;">
           ${getInitials(user.name)}
         </div>
       </div>
@@ -553,48 +360,48 @@ function createEditOverlay(user, index) {
         <div class="imgClose" onclick="closeOverlay()">
           <img src="./img/close.png" alt="" />
         </div>
-        <input
-          class="iconPerson"
-          type="text"
-          id="editUserName"
-          placeholder="Name"
-          value="${user.name}"
-          required
-        /><br /><br />
-        <input
-          class="iconMail"
-          type="email"
-          id="editUserEmail"
-          placeholder="E-Mail"
-          value="${user.email}"
-          required
-        /><br /><br />
-        <input
-          class="iconCall"
-          type="text"
-          id="editUserPhone"
-          placeholder="Telefon"
-          value="${user.phone}"
-          required
-        /><br /><br />
+        <div class="center">
+          <input
+            class="iconPerson"
+            type="text"
+            id="editUserName"
+            placeholder="Name"
+            value="${user.name}"
+            required/>
 
-        <input
-          class="iconLock"
-          type="password"
-          id="editUserPassword"
-          placeholder="new Password"
-        /><br /><br />
-        <!-- Passwort-Eingabe hinzugefügt -->
-        <div class="delete-save-btn">
+          <input
+            class="iconMail"
+            type="email"
+            id="editUserEmail"
+            placeholder="E-Mail"
+            value="${user.email}"
+            required/>
+
+          <input
+            class="iconCall"
+            type="text"
+            id="editUserPhone"
+            placeholder="Telefon"
+            value="${user.phone}"
+            required/>
+
+          <input
+            class="iconLock"
+            type="password"
+            id="editUserPassword"
+            placeholder="new Password"
+            required/>
+
+          <!-- Passwort-Eingabe hinzugefügt -->
+          <div class="delete-save-btn">
           <button
             class="delete-btn"
-            onclick="deleteUser('${user.name}'), clearUserInfo()"
-          >
-            Delete
-          </button>
+            onclick="deleteUser('${user.name}'), clearUserInfo()">
+            Delete</button>
           <button class="save-btn" onclick="editUser(${index})">
             Save <img class="imgCheck" src="./img/check.png" alt="" />
           </button>
+          </div>
         </div>
       </div>
     </div>
@@ -672,19 +479,3 @@ function getInitials(name) {
   const initials = names.map((name) => name.charAt(0)).join("");
   return initials;
 }
-// Funktion zum Anpassen der Sichtbarkeit der Elemente basierend auf der Bildschirmbreite
-function adjustVisibility() {
-  // Überprüfen der Bildschirmbreite
-  if (window.innerWidth <= 530) {
-    // Bild für Mobilgeräte anzeigen, Button ausblenden
-    document.querySelector(".addNewUserPic").style.display = "block";
-    document.querySelector(".addUserButtonDiv button").style.display = "none";
-  } else {
-    // Button für Desktop anzeigen, Bild ausblenden
-    document.querySelector(".addNewUserPic").style.display = "none";
-    document.querySelector(".addUserButtonDiv button").style.display = "block";
-  }
-}
-
-// Event-Listener hinzufügen, um die Sichtbarkeit anzupassen, wenn sich die Bildschirmgröße ändert
-window.addEventListener("resize", adjustVisibility);
